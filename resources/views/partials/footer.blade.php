6373c0c1760787408ddcc67ee6d381847c048356
SagarMhzn <maharzansagar4u@gmail.com>
2023-07-11 15:04:55 +0545

    footer partials and socials added

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,45 @@
+<div class="footer-top">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-3 col-md-6 footer-info">
+                <h3>REEEP</h3>
+                <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita
+                    valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet
+                    proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Partners</a></li>
+                    <li><a href="#">News and Events</a></li>
+                    <li><a href="#">Knowledge and Resources</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-contact">
+                <h4>Contact Us</h4>
+                <p>
+                    location<br>
+                    city<br>
+                    Nepal <br>
+                    <strong>Phone:</strong> +977 1234567890<br>
+                    <strong>Email:</strong> info@example.com<br>
+                </p>
+
+                <div class="social-links">
+                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                </div>
+
+            </div>
+
+            
+        </div>
+    </div>
+</div>
--- resources/views/partials/home-menu-item.blade.php
+++ resources/views/partials/home-menu-item.blade.php
@@ -1,10 +1,29 @@
-@if(isset($menuItem['children']))
+{{-- @if(isset($menuItem['children']))
     <li class="nav-item dropdown">
         <a class="nav-link" href="{{ $menuItem['slug'] }}">
             {{ $menuItem['title'] }}
             @if(count($menuItem['children']) > 0)
                 <div class="dropdown-toggle"></div>
             @endif
+        </a> 
+        <ul class="dropdown-menu">
+            @foreach($menuItem['children'] as $childItem)
+                @include('partials.home-menu-item', ['menuItem' => $childItem])
+            @endforeach
+        </ul>
+    </li>
+@else
+    <li class="nav-item">
+        <a class="nav-link" href="{{ $menuItem['slug'] }}">{{ $menuItem['title'] }}</a>
+    </li>
+@endif --}}
+
+
+@if(isset($menuItem['children']) && count($menuItem['children']) > 0)
+    <li class="nav-item dropdown">
+        <a class="nav-link" href="{{ $menuItem['slug'] }}">
+            {{ $menuItem['title'] }}
+            <div class="dropdown-toggle"></div>
         </a> 
         <ul class="dropdown-menu">
             @foreach($menuItem['children'] as $childItem)
